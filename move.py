9c7457427049656d08140516d8a33628e764e3df
Semyon Aronov <bigsem89@gmail.com>
2020-06-11 17:40:40 +0300

    Replace Print statements to logging module.

--- move.py
+++ move.py
@@ -5,21 +5,25 @@ import pygetwindow as gw
 from random_word import RandomWords
 import keyboard
 
+import logging
+logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s-%(levelname)s-%(message)s')
+#logging.disable(logging.CRITICAL)
+logging.info('start of program')
 
 while keyboard.is_pressed('esc') == False:
 
     counter = 0
     t = random.randrange(1, 30, 3)
     rc = random.randint(2, 20)
-    print('waiting for', t, 'seconds')
+    logging.info(f'Waiting for {t} seconds')
     time.sleep(t)
     task = random.randrange(1,4,1)
-    print('task is', task)
+    logging.info(f'task is {task}')
     if task == 1:
-        print(rc, 'movements')
+        logging.info(f'{rc} movements')
         while rc > counter and keyboard.is_pressed('esc') == False:
             counter += 1
-            print('counter is', counter)
+            logging.info(f'counter is {counter}')
             y = random.randrange(0, 764)
             x = random.randrange(0, 1360)
             duration = random.uniform(0, 5)
@@ -28,12 +32,14 @@ while keyboard.is_pressed('esc') == False:
         try:
             win = gw.getWindowsWithTitle('Блокнот')[0]
             win.activate()
-            print('some text printing')
+            logging.info('some text printing')
             r = RandomWords()
             rword = r.get_random_word() + " "
             pyautogui.typewrite((rword), interval=0.2)
         except:
             pyautogui.typewrite('Hello world ', interval=0.2)
     if task == 3:
-            print('pressing ALT+TAB')
+            logging.info('pressing ALT+TAB')
             pyautogui.hotkey('alt', 'tab')
+
+logging.info('end of program')
